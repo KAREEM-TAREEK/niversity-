f240954d28dbce50c218e320ef666f36f89d801b
KAREEM-TAREEK <kareemora9@gmail.com>
2023-03-17 03:46:03 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,425 @@
+
+
+function App() {
+  return (
+ 
+   <>
+
+<section className="home">
+    <div className="w">
+      <div  className="e">
+        {" "}
+        <i  className="fa-solid fa-building-columns" />
+        University
+      </div>
+      <div className="r">
+        <input type="checkbox" id="check" />
+        <label htmlFor="check" className="checkbtn">
+          <i className="fa fa-bars"> </i>
+        </label>
+        <ul>
+          <input type="checkbox" id="check" />
+          <label htmlFor="check" className="checkbtn">
+            <i className="fa-regular fa-circle-xmark" />
+          </label>
+          <li>
+            <a href="#q">HOME</a>
+          </li>
+          <li>
+            <a href="#kk">CONTENT</a>
+          </li>
+          <li>
+            <a href="#l">LOGIN</a>
+          </li>
+          <li>
+            <a href="#ttt">HELP</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div className="pragraph">
+      <button >University </button>
+      <p>Hello To My website </p>
+    </div>
+  </section>
+  <section  className="great">
+    Great feature
+  </section>
+  <section id="q" className="pagsmall">
+    <div className="pag">
+      <div className="t">
+        <i className="fa-solid fa-graduation-cap" />
+      </div>
+      <div className="scholarship facility">scholarship facility</div>
+      <p className="lorem">
+        We will guarantee you the best grants at the cheapest internationally
+        documented{" "}
+      </p>
+    </div>
+    <div className="pag">
+      <div className="t">
+        <i className="fa-solid fa-certificate" />
+      </div>
+      <div className="scholarship facility">scientific certificates</div>
+      <p className="lorem">
+        We will guarantee you the best grants at the cheapest internationally
+        documented{" "}
+      </p>
+    </div>
+    <div className="pag">
+      <div className="t">
+        <i className="fa-brands fa-ups" />
+      </div>
+      <div className="scholarship facility">Expenses and costs</div>
+      <p className="lorem">
+        We will guarantee you the best grants at the cheapest internationally
+        documented{" "}
+      </p>
+    </div>
+  </section>
+  <section  className="great">
+    our popular courses
+  </section>
+  <section id="kk" className="courses">
+    <div className="course">
+      <div className="imgs">
+        <img src="./img/img.6.jpg" alt="" />
+        <div className="price">
+          <h3>
+            {" "}
+            <i className="fa-solid fa-dollar-sign" />
+            100
+          </h3>
+        </div>
+      </div>
+      <div className="update">updated 21/8/21</div>
+      <div className="title">CSS BEGINNERS COURSE</div>
+      <div className="star">
+        {" "}
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        (233)
+      </div>
+    </div>
+    {/*2*/}
+    <div className="course">
+      <div className="imgs">
+        <img src="./img/img.5.jpg" alt="" />
+        <div className="price">
+          <h3>
+            {" "}
+            <i className="fa-solid fa-dollar-sign" />
+            800
+          </h3>
+        </div>
+      </div>
+      <div className="update">updated 21/8/21</div>
+      <div className="title">Cecurity BEGINNERS COURSE</div>
+      <div className="star">
+        {" "}
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        (233)
+      </div>
+    </div>
+    {/*3*/}
+    <div className="course">
+      <div className="imgs">
+        <img src="./img/img.4.jpg" alt="" />
+        <div className="price">
+          <h3>
+            {" "}
+            <i className="fa-solid fa-dollar-sign" />
+            100
+          </h3>
+        </div>
+      </div>
+      <div className="update">updated 21/8/21</div>
+      <div className="title">Html BEGINNERS COURSE</div>
+      <div className="star">
+        {" "}
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        (233)
+      </div>
+    </div>
+    {/*4*/}
+    <div className="course">
+      <div className="imgs">
+        <img src="./img/img.3.jpg" alt="" />
+        <div className="price">
+          <h3>
+            {" "}
+            <i className="fa-solid fa-dollar-sign" />
+            400
+          </h3>
+        </div>
+      </div>
+      <div className="update">updated 21/8/21</div>
+      <div className="title">Vs BEGINNERS COURSE</div>
+      <div className="star">
+        {" "}
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        (233)
+      </div>
+    </div>
+    {/*5*/}
+    <div className="course">
+      <div className="imgs">
+        <img src="./img/img.2.jpg" alt="" />
+        <div className="price">
+          <h3>
+            {" "}
+            <i className="fa-solid fa-dollar-sign" />
+            500
+          </h3>
+        </div>
+      </div>
+      <div className="update">updated 21/8/21</div>
+      <div className="title">PYTHON BEGINNERS COURSE</div>
+      <div className="star">
+        {" "}
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        (233)
+      </div>
+    </div>
+    {/*6*/}
+    <div className="course">
+      <div className="imgs">
+        <img src="./img/img.1.jpg" alt="" />
+        <div className="price">
+          <h3>
+            {" "}
+            <i className="fa-solid fa-dollar-sign" />
+            600
+          </h3>
+        </div>
+      </div>
+      <div className="update">updated 21/8/21</div>
+      <div className="title">Java BEGINNERS COURSE</div>
+      <div className="star">
+        {" "}
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        (233)
+      </div>
+    </div>
+    {/*2*/}
+    <div className="course">
+      <div className="imgs">
+        <img src="./img/img.5.jpg" alt="" />
+        <div className="price">
+          <h3>
+            {" "}
+            <i className="fa-solid fa-dollar-sign" />
+            700
+          </h3>
+        </div>
+      </div>
+      <div className="update">updated 21/8/21</div>
+      <div className="title">CSS BEGINNERS COURSE</div>
+      <div className="star">
+        {" "}
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        (233)
+      </div>
+    </div>
+    {/*3*/}
+    <div className="course">
+      <div className="imgs">
+        <img src="./img/img.4.jpg" alt="" />
+        <div className="price">
+          <h3>
+            {" "}
+            <i className="fa-solid fa-dollar-sign" />
+            400
+          </h3>
+        </div>
+      </div>
+      <div className="update">updated 21/8/21</div>
+      <div className="title">CSS BEGINNERS COURSE</div>
+      <div className="star">
+        {" "}
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        (233)
+      </div>
+    </div>
+    {/*4*/}
+    <div className="course">
+      <div className="imgs">
+        <img src="./img/img.3.jpg" alt="" />
+        <div className="price">
+          <h3>
+            {" "}
+            <i className="fa-solid fa-dollar-sign" />
+            500
+          </h3>
+        </div>
+      </div>
+      <div className="update">updated 21/8/21</div>
+      <div className="title">CSS BEGINNERS COURSE</div>
+      <div className="star">
+        {" "}
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        <i className="fa-solid fa-star" />
+        (233)
+      </div>
+    </div>
+  </section>
+  <section  id="l" className="great">
+    Join The Family
+  </section>
+  <section className="join">
+    <form className="lol" action="">
+      <div className="create">CREATE FREE ACCOUNT NOW</div>
+      <input type="text" placeholder="NAME" />
+      <input type="text" placeholder="Your Email" />
+      <input type="text" placeholder="Phone Number" />
+      <div className="btn">
+        <button>join</button>
+      </div>
+    </form>
+  </section>
+  <section  className="great">
+    Some experts
+  </section>
+  <section className="rating">
+    <div  className="ratings">
+      <img src="./img/img.7.jpg" alt="vv" />
+      <div className="starr">
+        <h2>jo tramp</h2>
+      </div>
+      <div className="icon">
+        <i className="fa-brands fa-facebook" />
+        <i className="fa-brands fa-instagram" />
+        <i className="fa-brands fa-linkedin" />
+      </div>
+    </div>
+    <div  className="ratings">
+      <img src="./img/img.8.jpg" alt="vv" />
+      <div className="starr">
+        <h2>serk lery</h2>
+      </div>
+      <div className="icon">
+        <i className="fa-brands fa-facebook" />
+        <i className="fa-brands fa-instagram" />
+        <i className="fa-brands fa-linkedin" />
+      </div>
+    </div>
+    <div className="ratings">
+      <img src="./img/img.9.jpg" alt="vv" />
+      <div className="starr">
+        <h2>adam jon</h2>
+      </div>
+      <div className="icon">
+        <i className="fa-brands fa-facebook" />
+        <i className="fa-brands fa-instagram" />
+        <i className="fa-brands fa-linkedin" />
+      </div>
+    </div>
+  </section>
+  {/*  --------------------                             -*/}
+  <header>{/* Content */}</header>
+  <main>{/* Content */}</main>
+  <footer id="ttt" className="footer">
+    <div className="footer__addr">
+      <h1 className="footer__logo">Something</h1>
+      <h2>Contact</h2>
+      <address>
+        5534 Somewhere In. The World 22193-10212
+        <br />
+        <a className="footer__btn" href="mailto:example@gmail.com">
+          Email Us
+        </a>
+      </address>
+    </div>
+    <ul className="footer__nav">
+      <li className="nav__item">
+        <h2 className="nav__title">Media</h2>
+        <ul className="nav__ul">
+          <li>
+            <a href="#">Online</a>
+          </li>
+          <li>
+            <a href="#">Print</a>
+          </li>
+          <li>
+            <a href="#">Alternative Ads</a>
+          </li>
+        </ul>
+      </li>
+      <li className="nav__item nav__item--extra">
+        <h2 className="nav__title">Technology</h2>
+        <ul className="nav__ul nav__ul--extra">
+          <li>
+            <a href="#">Hardware Design</a>
+          </li>
+          <li>
+            <a href="#">Software Design</a>
+          </li>
+          <li>
+            <a href="#">Digital Signage</a>
+          </li>
+          <li>
+            <a href="#">Automation</a>
+          </li>
+          <li>
+            <a href="#">Artificial</a>
+          </li>
+          <li>
+            <a href="#">IoT</a>
+          </li>
+        </ul>
+      </li>
+      <li className="nav__item">
+        <h2 className="nav__title">Legal</h2>
+        <ul className="nav__ul">
+          <li>
+            <a href="#">Privacy Policy</a>
+          </li>
+          <li>
+            <a href="#">Terms of Use</a>
+          </li>
+          <li>
+            <a href="#">Sitemap</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+    <div className="legal">
+      <p>© 2023 Something. All rights reserved.</p>
+      <div className="legal__links">
+        <span>
+          Made with <span className="heart">♥</span> remotely from Anywhere
+        </span>
+      </div>
+    </div>
+  </footer>
+   </>
+  
+  );
+}
+
+export default App;
